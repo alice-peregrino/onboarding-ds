3d0a518f2c48a5473100a8fb14af17dd08db47c5
Alice Peregrino <aapp@cin.ufpe.br>
2023-03-28 11:52:12 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='I'm building an end-to-end recommendation system project. ',
+    author='Ana Alice Peregrino Pinto',
+    license='MIT',
+)
